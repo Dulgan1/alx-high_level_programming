fef46330750b81b1136e546a5ea16d29b25c7bba
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2022-12-01 04:22:09 -0800

    Changed flow

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,18 +6,20 @@ strg2 = "and is greater than 5"
 strg3 = "and is less than 6 and not 0"
 
 num = number % 10
-if number > 1:
-    if num > 5:
-        print(strg.format(number, num) + ' ' + strg2)
-    elif (num < 6) and (number != 0):
-        print(strg.format(number, num) + ' ' + strg3)
-    elif number == 0:
-        print("Last digit of 0 is 0 and is 0")
-    else:
-        print("{}".format(number))
-else:
+#if number > 1:
+if num > 5:
+    print(strg.format(number, num) + ' ' + strg2)
+elif num < 6 and number != 0:
+    print(strg.format(number, num) + ' ' + strg3)
+elif number == 0:
+    print("Last digit of 0 is 0 and is 0")
+#else:
+#    print("{}".format(number))
+elif number < 0::
     number = number * -1
     num = number % 10
     num = num * -1
     number = number * -1
     print(strg.format(number, num) + ' ' + strg3)
+else:
+    print("{}".format(number))
