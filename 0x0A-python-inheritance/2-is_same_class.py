cec0b5f31e6fd5d7a149845a5a4ecb2256c19773
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-01-09 16:40:56 -0800

    Docstring updated

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,4 +7,7 @@ returns true if the passed objects is instance of specified class
 """
 
 def is_same_class(obj, a_class):
+    """ Return:
+    True if object is exactly an instance of the class
+    """
     return type(obj) == a_class
