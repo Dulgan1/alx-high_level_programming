6d1ebf5e0e3489831384bce75f6da98dc4dfa888
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-01-09 16:26:53 -0800

    Initial commit for  task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/user/bin/python3
+"""
+Module 1-my_list
+
+Contains class MyList
+
+inherits from list
+"""
+
+class MyList(list):
+    """inherits from class list
+    methods:
+    print_sorted(self)
+    """
+    def print_sorted(self):
+        """ prints list ofint all sorted in ascending order"""
+        print(sorted(self))
