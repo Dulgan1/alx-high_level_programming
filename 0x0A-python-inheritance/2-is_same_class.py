8f2dcb60cad45d6b02c1657d845e7a411103361c
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-01-09 16:36:34 -0800

    Initial commot for task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 2-is_same_class
+
+Contains method is_same_class
+returns true if the passed objects is instance of specified class
+"""
+
+def is_same_class(obj, a_class):
+    return type(obj) == a_class
