1bb276b37bcf66e4c843f7e4168bf6ed0c2793cc
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-01-11 07:46:47 -0800

    Conforming to pep8

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,6 +4,7 @@
 Contains method lookup that get list of attributes or methods of an object
 """
 
+
 def lookup(obj):
-    """ returns list of object's attribute and methods """
+    """returns list of object's attribute and methods"""
     return dir(obj)
