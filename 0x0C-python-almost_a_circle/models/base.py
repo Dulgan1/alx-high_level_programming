681298a90bd2193824c4f8d91fc3285e116ab246
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-01-15 14:59:17 -0800

    Base class created

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module contaons class Base
+
+Contains class atttibute __nb_object
+Returns JSON rep of list dictionary
+Saves JSON string of obejct to a file
+Return list of instances
+Saves to CSV file and loads to CSV file
+More...
+"""
+
+
+class Base:
+    """
+    defines class Base
+
+
+    Class Attributes:
+        (int) __nb_objects
+    Methods:
+        __init__(self, id=None)
+    Class Methods:
+        save_to_file(cls, list_obj) save_to_file_csv(cls, list_obj)
+        create(cls, **dictionary)
+        load_from_file(cls) load_from_file_csv(cls)
+    Static Methods:
+        to_json_string(list_dictionaries) from_json_string(json_string)
+    """
+    __nb_objects = 0
+
+   def __init__(self, id=None):
+        if id is None
+            __nb_object += 1
+            self.id = __nb_objects
+        else:
+            self.id = id
