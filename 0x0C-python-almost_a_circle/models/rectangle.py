0203b66ed5779fad2aaac5d5c88bacaf5ae70257
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-01-17 11:44:30 -0800

    Changed raise error messages

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -66,7 +66,7 @@ class Rectangle(Base):
     def x(self, value):
         """sets value to __x"""
         if type(value) is not int:
-            raise TypeError("x  must be an integer")
+            raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
@@ -84,7 +84,7 @@ class Rectangle(Base):
     def width(self, value):
         """sets value to __width"""
         if type(value) is not int:
-            raise TypeError("width must be integer")
+            raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
@@ -93,9 +93,9 @@ class Rectangle(Base):
     def height(self, value):
         """sets value to __height"""
         if type(value) is not int:
-            raise TypeError("height must be integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     def area(self):
