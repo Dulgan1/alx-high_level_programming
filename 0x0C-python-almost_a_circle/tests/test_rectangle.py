2817602bd1a7b620bfa9805ec3bb8974b6e618c1
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-01-17 17:48:01 -0800

    Updated tests files

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -16,12 +16,12 @@ Rectangle = rectangle.Rectangle
 
 
 class TestPep8(unittest.TestCase):
-    """Pep8 models/rectangle.py & tests/test_models/test_rectangle.py"""
+    """Pep8 models/rectangle.py & tests/test_rectangle.py"""
     def test_pep8(self):
         """Pep8"""
         style = pep8.StyleGuide(quiet=False)
         errors = 0
-        files = ["models/rectangle.py", "tests/test_models/test_rectangle.py"]
+        files = ["models/rectangle.py", "tests/test_rectangle.py"]
         errors += style.check_files(files).total_errors
         self.assertEqual(errors, 0, 'Need to fix Pep8')
 
