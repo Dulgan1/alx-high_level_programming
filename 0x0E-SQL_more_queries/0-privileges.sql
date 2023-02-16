887bccbf2122bbca2a84c53b5ae4087743853d30
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-02-15 16:52:25 -0800

    Correected typo

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Show grants/privileges given to users
-SHOW GRANT FOR 'user_0d_1'@'localhost';
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+-- Shows grants/privileges given to users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
