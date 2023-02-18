c7a4c9741ab7086fff531cf53eee9474eeb7d14a
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-02-18 09:34:13 -0800

    Initial commit for task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates user with all privileges, if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* FOR 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
