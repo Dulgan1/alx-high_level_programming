6a36f0b32e536fea20fe74313954eabf9067150e
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-02-19 13:04:40 -0800

    Initialcommit for task 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,3 @@
 -- Creates  new table force_name, if it doesn't exist
+-- with columns id and name cannot be null when adding records
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
