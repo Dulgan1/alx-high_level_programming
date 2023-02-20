bdb6bc695d45c43fdef60c7b666c76e0f310c27c
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-02-20 03:24:47 -0800

    Corrected typo

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Displays alll cities in hbtn_0d_usa
 -- in format, cities.id cities.name state.name
 
-SELECT cities.id, cities.name, states.name FROM cities JOIN  states WHERE cities.state_id = state.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN  states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
