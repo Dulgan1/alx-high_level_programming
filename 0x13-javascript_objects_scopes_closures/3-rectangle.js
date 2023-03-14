e424ffa6c4b20511790f55a6f502379123cf34e5
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-14 16:59:26 -0700

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+  print () {
+    for (let y = 0; y <= this.height; y++) {
+      for (let x = 0; x <= this.width; x++) {
+        console.log('X');
+}
