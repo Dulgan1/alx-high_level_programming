dee3e90ce0a722d8b88bce5d8953ddf790fa7027
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-14 19:16:33 -0700

    Changed argv index

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let fileA = process.argv[2];
-let fileB = process.argv[3];
-let fileC = process.argv[4];
+let fileA = process.argv[1];
+let fileB = process.argv[2];
+let fileC = process.argv[3];
 const fs = require('fs');
 let textA = fs.readFileSync(fileA, 'utf8');
 let textB = fs.readFileSync(fileB, 'utf8');
