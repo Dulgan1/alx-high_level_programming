b9b642c06927071decdb8c3fc2a83a0b3f790440
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-16 00:08:37 -0700

    confforming to semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const strArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let str in strArray) {
-  console.log(strArray[str]);
+for (const str of strArray) {
+  console.log(str);
 }
