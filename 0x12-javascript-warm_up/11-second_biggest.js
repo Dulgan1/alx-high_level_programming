e47bd79b8170997f4a6777596fa8113f1e998d8f
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-16 03:44:59 -0700

    Conforming to semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const numArray = process.argv.slice(2);
 
 function secondBiggest (array) {
   if (array.length > 1) {
-    array.sort(function (x, y) { return x - y });
+    array.sort(function (x, y) { return x - y; });
     return (array[array.length - 2]);
   } else {
     return (0);
