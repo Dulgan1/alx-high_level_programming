07bb9933c45551d1ab4f10514e9a439e08f7578f
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-16 03:41:45 -0700

    Initial commit

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const numArray = process.argv.slice(2);
+
+function secondBiggest (array) {
+  if (array.length > 1) {
+    array.sort(function (x, y) { return x - y });
+    return (array[array.length - 2]);
+  } else {
+    return (0);
+  }
+}
+
+console.log(secondBiggest(numArray));
