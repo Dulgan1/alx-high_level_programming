c1ce25c1b8509f0519f5af093efdeac428277c5a
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-16 00:38:31 -0700

    conforming to semistandard

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,8 +4,8 @@ const arg2 = process.argv[3];
 const x = +arg1;
 const y = +arg2;
 
-function add(a, b) {
-  return  a + b;
+function add (a, b) {
+  return a + b;
 }
 
 if (!isNaN(x) && !isNaN(y)) {
