c7c31b442f74be0199bcc6d71383ca84636fa264
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-16 00:15:15 -0700

    Initial commit

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+const x = +arg;
+if (!isNaN(x)) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurences');
+}
