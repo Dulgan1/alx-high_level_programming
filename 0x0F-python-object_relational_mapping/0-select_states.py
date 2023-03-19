4ad722379f6e61dae095667f7fd829ad23d484a5
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-19 03:40:10 -0700

    Fix typo

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import MySQLdb
 from sys import argv
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", prot=3306, user=arv[1],
                             passwd=argv[2], db=argv[3])
     curs = db.cursor()
