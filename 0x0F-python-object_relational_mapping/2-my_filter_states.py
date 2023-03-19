951aa7a2992cd72cb4e7f58fe111812015c41562
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-19 05:00:52 -0700

    Fix typo

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=[2], db=argv[3])
+                         passwd=argv[2], db=argv[3])
 
     curs = db.cursor()
     curs.execute("""SELECT * FROM states
