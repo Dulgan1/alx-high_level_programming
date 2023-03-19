a66d81aba09780859a86ac0259e755efcac81778
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-19 06:17:21 -0700

    Inotoal commit

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,12 +12,12 @@
 import MySQLdb
 from sys import argv
 
-if  __name__ == "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                 passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3])
 
     curs = db.cursor()
-    curs.execute("""SELECT * FROM states WHERE name='{:s}' 
+    curs.execute("""SELECT * FROM states WHERE name='%s'
                  ORDER BY id ASC""".format(argv[4]))
 
     for row in curs.fetchall():
