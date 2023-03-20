53b5a33e3d3d14d4f047def7ca7fab4a8a8e1cc3
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-20 14:31:43 -0700

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+    Script disolays first State object from a database
+    argv[1] -> user to access db
+    argv[2] -> password to access db
+    argv[3] -> database db
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    eng_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+
+    engine = create_engine(eng_str.format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    if session.query(State).order_by(State.id).first() as instance:
+        print("{:d}: {:s}".format(instance.id, instance.name))
+    else:
+        print("Nothing")
+
+    session.close()
