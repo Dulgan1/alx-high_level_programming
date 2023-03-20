7b2c1f2d93c216aa6ef3be2edbe7cd5c8d1add98
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-20 14:44:42 -0700

    String variable 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-    Script disolays first State object from a database
+    Script displays first State object from a database
     argv[1] -> user to access db
     argv[2] -> password to access db
     argv[3] -> database db
@@ -13,14 +13,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    eng_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    eng_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1],
+                                                               argv[2],
+                                                               argv[3])
 
-    engine = create_engine(eng_str.format(argv[1], argv[2], argv[3]))
+    engine = create_engine(eng_str, pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
-
     session = Session()
     instance = session.query(State).order_by(State.id).first()
+
     if instance:
         print("{:d}: {:s}".format(instance.id, instance.name))
     else:
