6732e515209a198aba667a2921134d3f790f4f21
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-04-25 18:51:34 -0700

    initial commot

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+
+request(url, (error, response, body) => {
+  if (error) {
+    return console.log('error: ' + error);
+  }
+
+  const r = JSON.parse(body).results;
+  for (const x in r) {
+    const characterS = r[x].characters;
+    for (const i in characterS) {
+      if (characterS[i].includes('18')) {
+        count++;
+      }
+    }
+  }
+});
