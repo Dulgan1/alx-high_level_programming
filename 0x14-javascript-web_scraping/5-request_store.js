a6d8a8077fb5cb6f1a6544e048778c0b543b6976
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-04-26 14:20:27 -0700

    Initial commit

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log("error: " + err);
+  } else {
+    fs.writeFile(file, body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
