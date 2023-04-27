3b5eda916172b28eaf2255e02802189bdbf41f6c
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-04-26 20:16:51 -0700

    Fix typo error

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-  if error {
+  if (error) {
     console.log(error)
   } else {
     let task = JSON.parse(body);
