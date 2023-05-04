224b22e174058b5f74b89b28c8d8123845c6e62a
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-05-04 06:01:17 -0700

    Removed to defer

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-const urllink = process.argv[2];
-
-request.get(urllink).on('response', (response) => {
-  console.log('code: ' + response.statusCode);
-});
