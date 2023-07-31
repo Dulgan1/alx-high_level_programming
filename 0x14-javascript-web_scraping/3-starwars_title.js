a6b0cb9b5c9d21719f67f85ee4068672c59ff468
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-07-31 18:42:19 +0100

    Semistandard:

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -9,7 +9,7 @@ request(url, (error, response, body) => {
     console.log(error);
   } else if (response.statusCode === 200) {
     body = JSON.parse(body);
-    console.log(body['title']);
+    console.log(body.title);
   } else {
     console.log('Error: ' + response.statusCode);
   }
