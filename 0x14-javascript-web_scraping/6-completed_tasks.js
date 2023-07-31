a3fd6d2c30e2c086e779fde67be47177afaf6c97
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-08-01 00:25:05 +0100

    Changed flow

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,20 +2,23 @@
 
 const url = process.argv[2];
 const request = require('request');
-const taskD = {};
-const list = [];
-request(url, function (error, response, body) {
-  if (error) {
-    console.error('error: ' + error);
-  }
-  const todoList = JSON.parse(body);
-  for (const i in todoList) {
-    const task = todoList[i];
-    if (task.completed === true) {
-      list.push(task.userId);
+let dic = {};
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    let tasks = JSON.parse(body);
+    for (const i in tasks) {
+      if (tasks[i].completed) {
+	if (dic[tasks[i].userId] === undefined) {
+	  dic[tasks[i].userId] = 1;
+	} else {
+	  dic[tasks[i].userId]++;
+	}
+      }
     }
+    console.log(dic);
+  } else {
+    console.log('Error code: ' + response.statusCode);
   }
-
-  list.forEach((i) => { taskD[i] = (taskD[i] || 0) + 1; });
-  console.log(taskD);
 });
