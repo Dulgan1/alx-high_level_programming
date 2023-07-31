3b7550809a5a87bb5a5999b4b156adabce03dafd
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-07-31 23:42:05 +0100

    Simplified witb Pipe

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,14 +5,4 @@ const file = process.argv[3];
 const request = require('request');
 const fs = require('fs');
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else if (response.statusCode === 200) {
-    fs.writeFile(file, body, 'utf8', (error) => {
-      console.log(error);
-    });
-  } else {
-    console.log('Error: ' + response.statusCode);
-  }
-});
+request(url).pipe(fs.createWriteStream(file));
