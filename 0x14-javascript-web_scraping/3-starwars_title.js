9b2d32cc4e0e449460375fca84de459e7e60cc50
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-07-31 18:33:32 +0100

    Initial commit

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+const request = require('request');
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(body[title]);
+  } else {
+    console.log('Error: ' + response.statusCode);
+  }
+});
