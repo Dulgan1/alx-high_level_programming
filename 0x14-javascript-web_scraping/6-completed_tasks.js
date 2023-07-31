f11dadc58a3ed1cd25284488dc8a14c8eb84dc13
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-07-31 23:59:30 +0100

    Initial commit

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const url = process.argv[2]
+const request = require('request');
+const taskD = {};
+const list = [];
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error: ' + error);
+  }
+  const todoList = JSON.parse(body);
+  for (const i in todoList) {
+    const task = todoList[i];
+    if (task.completed === true) {
+      list.push(task.userId);
+    }
+  }
+
+  list.forEach((i) => { taskD[i] = (taskD[i] || 0) + 1; });
+  console.log(taskD);
+});
