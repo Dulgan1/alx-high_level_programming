8ad6a1db6ce1663823ed27f0eaa4644cdc2f2960
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-08-05 23:35:18 +0100

    Initial commit

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,14 @@
+//  
+document.addEventListener('DOMContentLoaded', function () {
+  $('div#add_item').click(function () {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+
+  $('div#remove_item').click(function () {
+    $('ul.my_list li:last-child').remove();
+  });
+
+  $('div#clear_list').click(function () {
+    $('ul.my_list').empty('<li>Item</li>');
+  });
+});
